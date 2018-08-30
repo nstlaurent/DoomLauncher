f2e8265004599742e9ee1df75262216f0d66e219
Erick Tenorio <guynamederick@gmail.com>
2018-08-29 23:42:17 -0500

    Add more IWADs to automatically name
    The added IWADs are:
    
    -Freedoom: Phase 1 (FREEDOOM1)
    -Freedoom: Phase 2 (FREEDOOM2)
    -Chex Quest (CHEX)
    -Chex Quest 3 (CHEX3)
    -Hacx: Twitch 'n Kill (HACX)
    - Heretic Shareware (HERETIC1)
    -Strife Demo (STRIFE0)
    
    Tweak names of the commercial Heretic and Strife IWADs.
    
    Shortened ZDoom stats description to read by default on stats info.

--- DoomLauncher/Forms/StatsInfo.cs
+++ DoomLauncher/Forms/StatsInfo.cs
@@ -19,7 +19,7 @@ namespace DoomLauncher
             pbInfo1.Image = DoomLauncher.Properties.Resources.bon2b;
 
             lblZdoom.Text = "For all ZDoom based ports. Uses save games to parse statistics. This means statistics cannot be read for the last level of an episode. " +
-                "Items are not available on ZDoom, Zandronum, and versions of GZDoom prior to 3.5.0. Statistics will be recorded when the game is saved or when an auto save is generated.";
+                "Items are not available on ZDoom, Zandronum, and GZDoom pre-3.5.0. Statistics will be recorded when the game is saved or when an auto save is generated.";
             lblBoom.Text = "Uses the -levelstat parameter and parses the generated levelstat.txt. All statistics will be recorded when the game has exited.";
             lblCNDoom.Text = "Uses the -printstats parameter and parses the generated stdout.txt. All statistics will be recorded when the game has exited.";
         }
