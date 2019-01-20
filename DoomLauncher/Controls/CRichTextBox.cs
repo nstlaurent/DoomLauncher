f46b501dfd6fa152cd615b46f215ae1d0d2f36f8
Nick <stlaurent.nicholas@gmail.com>
2019-01-20 08:37:29 -0500

    rich text box to automatically handle link navigation and warning

---
+++ DoomLauncher/Controls/CRichTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoomLauncher.Controls
+{
+    public partial class CRichTextBox : RichTextBox
+    {
+        public CRichTextBox()
+        {
+            InitializeComponent();
+            WarnLinkClick = true;
+            LinkClicked += CRichTextBox_LinkClicked;
+        }
+
+        public bool WarnLinkClick { get; set; }
+
+        private void CRichTextBox_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            if (ShouldNavigate(e))
+                Process.Start(e.LinkText);
+        }
+
+        private bool ShouldNavigate(LinkClickedEventArgs e)
+        {
+            if (WarnLinkClick)
+            {
+                return MessageBox.Show(this, string.Format("You are about to navigate to:{1}{0} {1}{1}Continue?", e.LinkText, Environment.NewLine),
+                    "Navigate", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK;
+            }
+
+            return true;
+        }
+    }
+}
