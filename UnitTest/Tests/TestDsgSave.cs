69dd9eba7f966ee5a8f3fbae8ba8f75df6c3c12a
Nick <stlaurent.nicholas@gmail.com>
2019-01-26 16:25:41 -0500

    save game unit tests

---
+++ UnitTest/Tests/TestDsgSave.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using DoomLauncher.SaveGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Tests
+{
+    [TestClass]
+    public class TestDsgSave
+    {
+        [TestMethod]
+        public void TestPrBoomSave()
+        {
+            string save1 = "prboomsave1.dsg";
+            DeleteSaveFile(save1);
+            File.Copy(Path.Combine("Resources", save1), save1);
+
+            DsgSaveGameReader reader = new DsgSaveGameReader(save1);
+
+            Assert.AreEqual("NEW2", reader.GetName());
+        }
+
+        [TestMethod]
+        public void TestChocolateDoomSave()
+        {
+            string save1 = "chocosave1.dsg";
+            DeleteSaveFile(save1);
+            File.Copy(Path.Combine("Resources", save1), save1);
+
+            DsgSaveGameReader reader = new DsgSaveGameReader(save1);
+
+            Assert.AreEqual("CHOCOTESTTESTTESTTESTTE", reader.GetName());
+        }
+
+        private static void DeleteSaveFile(string save1)
+        {
+            if (File.Exists(save1))
+                File.Delete(save1);
+        }
+    }
+}
---
+++ UnitTest/Tests/TestZdoomSave.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using DoomLauncher.SaveGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest.Tests;
+
+namespace UnitTest.Tests
+{
+    [TestClass]
+    public class TestZdoomSave
+    {
+        [TestMethod]
+        public void TestZdoomSaveBinary()
+        {
+            string save1 = "zdoomsave_v1.zds";
+            DeleteSaveFile(save1);
+            File.Copy(Path.Combine("Resources", save1), save1);
+
+            ZDoomSaveGameReader reader = new ZDoomSaveGameReader(save1);
+
+            Assert.AreEqual("Autosave Aug 21 13:30", reader.GetName());
+        }
+
+        [TestMethod]
+        public void TestZdoomSaveJson()
+        {
+            string save1 = "zdoomsave_v2.zds";
+            DeleteSaveFile(save1);
+            File.Copy(Path.Combine("Resources", save1), save1);
+
+            ZDoomSaveGameReader reader = new ZDoomSaveGameReader(save1);
+
+            Assert.AreEqual("Autosave Aug 21 13:12", reader.GetName());
+        }
+
+        [TestMethod]
+        public void TestZdoomSaveJson_3_5()
+        {
+            string save1 = "zdoomsave_v3.zds";
+            DeleteSaveFile(save1);
+            File.Copy(Path.Combine("Resources", save1), save1);
+
+            ZDoomSaveGameReader reader = new ZDoomSaveGameReader(save1);
+
+            Assert.AreEqual("Autosave Aug 28 06:25", reader.GetName());
+        }
+
+        private static void DeleteSaveFile(string save1)
+        {
+            if (File.Exists(save1))
+                File.Delete(save1);
+        }
+    }
+}
