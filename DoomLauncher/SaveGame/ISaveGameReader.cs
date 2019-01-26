a28d8d4cebb2286d9e6015efa5f036913725c14f
Nick <stlaurent.nicholas@gmail.com>
2019-01-26 16:11:49 -0500

    save game reader interface

---
+++ DoomLauncher/SaveGame/ISaveGameReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomLauncher.SaveGame
+{
+    public interface ISaveGameReader
+    {
+        string GetName();
+    }
+}
