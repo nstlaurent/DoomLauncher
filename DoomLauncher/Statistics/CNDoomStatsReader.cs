5b6d5d2789935957858427ad9bb1f5f62418b6f3
Nick <stlaurent.nicholas@gmail.com>
2019-06-27 22:22:11 -0400

    use base stat reader

--- DoomLauncher/Statistics/CNDoomStatsReader.cs
+++ DoomLauncher/Statistics/CNDoomStatsReader.cs
@@ -1,4 +1,5 @@
 ﻿using DoomLauncher.Interfaces;
+using DoomLauncher.Statistics;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,42 +7,9 @@ using System.Text.RegularExpressions;
 
 namespace DoomLauncher
 {
-    public class CNDoomStatsReader : StatFileScanner, IStatisticsReader
+    public class CNDoomStatsReader : MultiLineStatReader
     {
-        public event NewStatisticsEventHandler NewStastics;
-
-        private readonly List<IStatsData> m_statistics = new List<IStatsData>();
-
-        public CNDoomStatsReader(IGameFile gameFile, string stdoutFile) : base(stdoutFile)
-        {
-            GameFile = gameFile;
-        }
-
-        public IGameFile GameFile { get; set; }
-
-        public string LaunchParameter { get { return "-printstats"; } }
-        public bool ReadOnClose { get { return true; } }
-
-        public void Start()
-        {
-            //nothing to do on start
-        }
-
-        public void Stop()
-        {
-            //nothing to do on stop
-        }
-
-        public void ReadNow()
-        {
-            ReadStatistics();
-        }
-
-        public string[] Errors
-        {
-            get { return m_errors.ToArray(); }
-        }
-
+        private static string s_statRegex = @"#+\w+#+Time:\d+:\d+.\d+Kills:\d+/\d+#+Items:\d+/\d+Secrets:\d+/\d+";
         private static ParseItem[] s_regexItems = new ParseItem[]
         {
             new ParseItem(@"\w+", string.Empty, "MapName"),
@@ -54,77 +22,11 @@ namespace DoomLauncher
             new ParseItem(@"/\d+", "/", "TotalSecrets")
         };
 
-        private void ReadStatistics()
+        public CNDoomStatsReader(IGameFile gameFile, string stdoutFile) : base(gameFile, stdoutFile, s_statRegex, s_regexItems)
         {
-            try
-            {
-                string statRegex = @"#+\w+#+Time:\d+:\d+.\d+Kills:\d+/\d+#+Items:\d+/\d+Secrets:\d+/\d+";
-                string text = File.ReadAllText(StatFile);
-                text = text.Replace(" ", string.Empty).Replace("\r\n", string.Empty).Replace("\n", string.Empty);
-                MatchCollection matches = Regex.Matches(text, statRegex, RegexOptions.Singleline);
-
-                foreach (Match match in matches)
-                {
-                    IStatsData stats = null;
-
-                    if (match.Success)
-                        stats = ParseLine(match.Value);
-
-                    if (stats != null)
-                    {
-                        stats.RecordTime = DateTime.Now;
-                        stats.GameFileID = GameFile.GameFileID.Value;
-
-                        //Revived monsters adds to kill count
-                        if (stats.KillCount > stats.TotalKills)
-                            stats.KillCount = stats.TotalKills;
-
-                        if (!m_statistics.Contains(stats))
-                        {
-                            m_statistics.Add(stats);
-
-                            if (NewStastics != null)
-                                NewStastics(this, new NewStatisticsEventArgs(stats));
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                m_errors.Add(string.Format("Unexpected exception: {0}{1}{2}", ex.Message, Environment.NewLine, ex.StackTrace));
-            }
+            
         }
 
-        private IStatsData ParseLine(string line)
-        {
-            StatsData ret = new StatsData();
-
-            foreach (ParseItem item in s_regexItems)
-            {
-                Match match = Regex.Match(line, item.RegexInput);
-
-                if (match.Success)
-                {
-                    line = ReplaceFirst(line, match.Value);
-                    SetStatProperty(ret, item, match.Value);
-                }
-                else
-                {
-                    m_errors.Add(string.Format("Failed to parse {0} from levelstat file.", item.DataSourceProperty));
-                }
-            }
-
-            return ret;
-        }
-
-        private static string ReplaceFirst(string text, string oldValue)
-        {
-            int idx = text.IndexOf(oldValue);
-
-            if (idx != -1)
-                return string.Concat(text.Substring(0, idx), text.Substring(idx + oldValue.Length));
-
-            return text;
-        }
+        public override string LaunchParameter { get { return "-printstats"; } }
     }
 }
