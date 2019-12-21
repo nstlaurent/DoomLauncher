489fa844177f04c59623f8c7ad6ecdea7e7d9eeb
Nick <stlaurent.nicholas@gmail.com>
2019-12-21 07:14:40 -0500

    initial game profile implementation

---
+++ DoomLauncher/Interfaces/IGameProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace DoomLauncher.Interfaces
+{
+    public interface IGameProfile
+    {
+        int GameProfileID { get; set; }
+        int? GameFileID { get; set; }
+        int? IWadID { get; set; }
+        int? SourcePortID { get; set; }
+        string Name { get; set; }
+        string SettingsMap { get; set; }
+        string SettingsSkill { get; set; }
+        string SettingsExtraParams { get; set; }
+        string SettingsFiles { get; set; }
+        string SettingsFilesSourcePort { get; set; }
+        string SettingsFilesIWAD { get; set; }
+        string SettingsSpecificFiles { get; set; }
+        bool SettingsStat { get; set; }
+        bool SettingsSaved { get; set; }
+    }
+}
