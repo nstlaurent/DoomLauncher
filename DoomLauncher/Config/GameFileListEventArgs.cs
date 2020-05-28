7eb61cf517925b5188c4df319fda8ae2c394c934
Nick <stlaurent.nicholas@gmail.com>
2020-05-28 14:22:53 -0400

    GameFileListEventArgs

---
+++ DoomLauncher/Config/GameFileListEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using DoomLauncher.Interfaces;
+using System.Collections.Generic;
+
+namespace DoomLauncher
+{
+    public class GameFileListEventArgs
+    {
+        public IEnumerable<IGameFile> GameFiles { get; set; }
+
+        public GameFileListEventArgs(IEnumerable<IGameFile> gameFiles)
+        {
+            GameFiles = gameFiles;
+        }
+    }
+}
