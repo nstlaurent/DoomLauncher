faa2df7a7ee87d2056ce4043fb8928b2063359d1
Nick <stlaurent.nicholas@gmail.com>
2020-06-20 12:13:11 -0400

    refactor

--- DoomLauncher/TabViews/IdGamesTabViewCtrl.cs
+++ DoomLauncher/TabViews/IdGamesTabViewCtrl.cs
@@ -76,7 +76,7 @@ namespace DoomLauncher
 
             if (IdGamesDataSource != null)
             {
-                base.SetDataSource(IdGamesDataSource.ToList());
+                base.SetDataSource(IdGamesDataSource);
             }
             else
             {
