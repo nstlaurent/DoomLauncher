eb41aef64b357106b8e327d5a7ca2bb831827b5e
Nick <stlaurent.nicholas@gmail.com>
2023-08-04 13:35:49 -0400

    fix titlebar dock

--- DoomLauncher/Forms/MetaDataForm.Designer.cs
+++ DoomLauncher/Forms/MetaDataForm.Designer.cs
@@ -143,11 +143,15 @@
             // titleBar
             // 
             this.titleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(54)))));
+            this.titleBar.CanClose = true;
+            this.titleBar.ControlBox = true;
+            this.titleBar.Dock = System.Windows.Forms.DockStyle.Fill;
             this.titleBar.ForeColor = System.Drawing.Color.White;
             this.titleBar.Location = new System.Drawing.Point(0, 0);
             this.titleBar.Margin = new System.Windows.Forms.Padding(0);
             this.titleBar.Name = "titleBar";
-            this.titleBar.Size = new System.Drawing.Size(644, 37);
+            this.titleBar.RememberNormalSize = true;
+            this.titleBar.Size = new System.Drawing.Size(645, 40);
             this.titleBar.TabIndex = 3;
             this.titleBar.Title = "Metadata";
             // 
