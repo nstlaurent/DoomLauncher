f986e647adb2c1598aadf5c4447b86eabdc62151
Nick <stlaurent.nicholas@gmail.com>
2023-08-10 10:27:12 -0400

    fix play random sizing for removing custom titlebar

--- DoomLauncher/Forms/PlayRandomForm.Designer.cs
+++ DoomLauncher/Forms/PlayRandomForm.Designer.cs
@@ -47,19 +47,19 @@
             // 
             this.cmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbType.FormattingEnabled = true;
-            this.cmbType.Location = new System.Drawing.Point(17, 14);
+            this.cmbType.Location = new System.Drawing.Point(19, 18);
             this.cmbType.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbType.Name = "cmbType";
-            this.cmbType.Size = new System.Drawing.Size(228, 24);
+            this.cmbType.Size = new System.Drawing.Size(256, 28);
             this.cmbType.TabIndex = 0;
             // 
             // btnOK
             // 
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(233, 192);
+            this.btnOK.Location = new System.Drawing.Point(262, 240);
             this.btnOK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(78, 28);
+            this.btnOK.Size = new System.Drawing.Size(88, 35);
             this.btnOK.TabIndex = 2;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -68,20 +68,20 @@
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(317, 192);
+            this.btnCancel.Location = new System.Drawing.Point(357, 240);
             this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(73, 28);
+            this.btnCancel.Size = new System.Drawing.Size(82, 35);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(252, 14);
+            this.btnGenerate.Location = new System.Drawing.Point(284, 18);
             this.btnGenerate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(140, 24);
+            this.btnGenerate.Size = new System.Drawing.Size(158, 30);
             this.btnGenerate.TabIndex = 4;
             this.btnGenerate.Text = "Generate";
             this.btnGenerate.UseVisualStyleBackColor = true;
@@ -90,9 +90,9 @@
             // lblText
             // 
             this.lblText.AutoSize = true;
-            this.lblText.Location = new System.Drawing.Point(17, 77);
+            this.lblText.Location = new System.Drawing.Point(19, 96);
             this.lblText.Name = "lblText";
-            this.lblText.Size = new System.Drawing.Size(45, 16);
+            this.lblText.Size = new System.Drawing.Size(58, 20);
             this.lblText.TabIndex = 5;
             this.lblText.Text = "Output";
             // 
@@ -100,7 +100,7 @@
             // 
             this.tblMain.ColumnCount = 1;
             this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tblMain.Controls.Add(this.panel1, 0, 1);
             this.tblMain.Controls.Add(this.titleBar, 0, 0);
             this.tblMain.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -108,9 +108,9 @@
             this.tblMain.Margin = new System.Windows.Forms.Padding(0);
             this.tblMain.Name = "tblMain";
             this.tblMain.RowCount = 2;
-            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tblMain.Size = new System.Drawing.Size(404, 262);
+            this.tblMain.Size = new System.Drawing.Size(454, 282);
             this.tblMain.TabIndex = 6;
             // 
             // panel1
@@ -123,19 +123,19 @@
             this.panel1.Controls.Add(this.btnOK);
             this.panel1.Controls.Add(this.btnCancel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 32);
+            this.panel1.Location = new System.Drawing.Point(0, 40);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(404, 230);
+            this.panel1.Size = new System.Drawing.Size(454, 242);
             this.panel1.TabIndex = 0;
             // 
             // chkShowPlayDialog
             // 
             this.chkShowPlayDialog.AutoSize = true;
-            this.chkShowPlayDialog.Location = new System.Drawing.Point(175, 50);
+            this.chkShowPlayDialog.Location = new System.Drawing.Point(197, 62);
             this.chkShowPlayDialog.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chkShowPlayDialog.Name = "chkShowPlayDialog";
-            this.chkShowPlayDialog.Size = new System.Drawing.Size(135, 20);
+            this.chkShowPlayDialog.Size = new System.Drawing.Size(157, 24);
             this.chkShowPlayDialog.TabIndex = 6;
             this.chkShowPlayDialog.Text = "Show Play Dialog";
             // 
@@ -144,10 +144,10 @@
             this.chkRandomMap.AutoSize = true;
             this.chkRandomMap.Checked = true;
             this.chkRandomMap.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkRandomMap.Location = new System.Drawing.Point(17, 50);
+            this.chkRandomMap.Location = new System.Drawing.Point(19, 62);
             this.chkRandomMap.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chkRandomMap.Name = "chkRandomMap";
-            this.chkRandomMap.Size = new System.Drawing.Size(152, 20);
+            this.chkRandomMap.Size = new System.Drawing.Size(180, 24);
             this.chkRandomMap.TabIndex = 1;
             this.chkRandomMap.Text = "Select Random Map";
             // 
@@ -161,17 +161,18 @@
             this.titleBar.Location = new System.Drawing.Point(0, 0);
             this.titleBar.Margin = new System.Windows.Forms.Padding(0);
             this.titleBar.Name = "titleBar";
-            this.titleBar.Size = new System.Drawing.Size(404, 32);
+            this.titleBar.RememberNormalSize = true;
+            this.titleBar.Size = new System.Drawing.Size(454, 40);
             this.titleBar.TabIndex = 1;
             this.titleBar.Title = "Play Random";
             // 
             // PlayRandomForm
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(404, 262);
+            this.ClientSize = new System.Drawing.Size(454, 282);
             this.Controls.Add(this.tblMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
