7ed1900c94cdc8f8a2a545910d70e2e72da9ea90
Nick <stlaurent.nicholas@gmail.com>
2023-09-02 09:48:19 -0400

    update styling

--- DoomLauncher/Forms/ScreenshotEditForm.Designer.cs
+++ DoomLauncher/Forms/ScreenshotEditForm.Designer.cs
@@ -36,51 +36,51 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
-            this.txtTitle = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.flpMap = new System.Windows.Forms.FlowLayoutPanel();
-            this.chkMap = new System.Windows.Forms.CheckBox();
             this.cmbMap = new System.Windows.Forms.ComboBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
             this.tblMain = new System.Windows.Forms.TableLayoutPanel();
             this.titleBar = new DoomLauncher.Controls.TitleBarControl();
+            this.chkMap = new DoomLauncher.CCheckBox();
             this.tblData.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
-            this.flpMap.SuspendLayout();
             this.tblMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // tblData
             // 
-            this.tblData.ColumnCount = 2;
-            this.tblData.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tblData.ColumnCount = 3;
+            this.tblData.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tblData.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
             this.tblData.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tblData.Controls.Add(this.label1, 0, 0);
+            this.tblData.Controls.Add(this.chkMap, 1, 1);
             this.tblData.Controls.Add(this.label2, 0, 2);
-            this.tblData.Controls.Add(this.txtDescription, 1, 2);
-            this.tblData.Controls.Add(this.flowLayoutPanel1, 1, 3);
-            this.tblData.Controls.Add(this.txtTitle, 1, 0);
+            this.tblData.Controls.Add(this.txtDescription, 2, 2);
+            this.tblData.Controls.Add(this.flowLayoutPanel1, 2, 3);
             this.tblData.Controls.Add(this.label3, 0, 1);
-            this.tblData.Controls.Add(this.flpMap, 1, 1);
+            this.tblData.Controls.Add(this.txtTitle, 2, 0);
+            this.tblData.Controls.Add(this.cmbMap, 2, 1);
             this.tblData.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tblData.Location = new System.Drawing.Point(4, 45);
-            this.tblData.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tblData.Location = new System.Drawing.Point(4, 36);
+            this.tblData.Margin = new System.Windows.Forms.Padding(4);
             this.tblData.Name = "tblData";
             this.tblData.RowCount = 4;
-            this.tblData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
-            this.tblData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.tblData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tblData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
             this.tblData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tblData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
-            this.tblData.Size = new System.Drawing.Size(792, 609);
+            this.tblData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tblData.Size = new System.Drawing.Size(703, 487);
             this.tblData.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 14);
+            this.label1.Location = new System.Drawing.Point(4, 11);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.Size = new System.Drawing.Size(33, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Title";
             // 
@@ -88,21 +88,21 @@
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 319);
+            this.label2.Location = new System.Drawing.Point(4, 255);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.Size = new System.Drawing.Size(75, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Description";
             // 
             // txtDescription
             // 
             this.txtDescription.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtDescription.Location = new System.Drawing.Point(154, 103);
-            this.txtDescription.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDescription.Location = new System.Drawing.Point(136, 82);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(4);
             this.txtDescription.Multiline = true;
             this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(634, 452);
+            this.txtDescription.Size = new System.Drawing.Size(563, 362);
             this.txtDescription.TabIndex = 2;
             // 
             // flowLayoutPanel1
@@ -111,19 +111,19 @@
             this.flowLayoutPanel1.Controls.Add(this.btnSave);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(150, 560);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(132, 448);
             this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(642, 49);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(571, 39);
             this.flowLayoutPanel1.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(526, 5);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(467, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Cancel";
             this.button1.UseVisualStyleBackColor = true;
@@ -131,73 +131,51 @@
             // btnSave
             // 
             this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnSave.Location = new System.Drawing.Point(406, 5);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Location = new System.Drawing.Point(359, 4);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(112, 35);
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
             this.btnSave.TabIndex = 1;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // txtTitle
-            // 
-            this.txtTitle.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.txtTitle.Location = new System.Drawing.Point(153, 11);
-            this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(636, 26);
-            this.txtTitle.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 63);
+            this.label3.Location = new System.Drawing.Point(3, 50);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 20);
+            this.label3.Size = new System.Drawing.Size(34, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Map";
             // 
-            // flpMap
-            // 
-            this.flpMap.Controls.Add(this.chkMap);
-            this.flpMap.Controls.Add(this.cmbMap);
-            this.flpMap.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flpMap.Location = new System.Drawing.Point(150, 49);
-            this.flpMap.Margin = new System.Windows.Forms.Padding(0);
-            this.flpMap.Name = "flpMap";
-            this.flpMap.Size = new System.Drawing.Size(642, 49);
-            this.flpMap.TabIndex = 7;
-            // 
-            // chkMap
-            // 
-            this.chkMap.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.chkMap.AutoSize = true;
-            this.chkMap.Location = new System.Drawing.Point(3, 8);
-            this.chkMap.Name = "chkMap";
-            this.chkMap.Size = new System.Drawing.Size(22, 21);
-            this.chkMap.TabIndex = 0;
-            this.chkMap.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.chkMap.UseVisualStyleBackColor = true;
-            this.chkMap.CheckedChanged += new System.EventHandler(this.chkMap_CheckedChanged);
-            // 
             // cmbMap
             // 
-            this.cmbMap.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.cmbMap.DisplayMember = "Name";
+            this.cmbMap.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cmbMap.Enabled = false;
             this.cmbMap.FormattingEnabled = true;
-            this.cmbMap.Location = new System.Drawing.Point(32, 5);
-            this.cmbMap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbMap.Location = new System.Drawing.Point(136, 47);
+            this.cmbMap.Margin = new System.Windows.Forms.Padding(4, 8, 4, 4);
             this.cmbMap.Name = "cmbMap";
-            this.cmbMap.Size = new System.Drawing.Size(238, 28);
+            this.cmbMap.Size = new System.Drawing.Size(563, 24);
             this.cmbMap.TabIndex = 7;
             this.cmbMap.TabStop = false;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtTitle.Location = new System.Drawing.Point(135, 8);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(3, 8, 3, 2);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(565, 22);
+            this.txtTitle.TabIndex = 5;
             // 
             // tblMain
             // 
             this.tblMain.ColumnCount = 1;
             this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 18F));
             this.tblMain.Controls.Add(this.tblData, 0, 1);
             this.tblMain.Controls.Add(this.titleBar, 0, 0);
             this.tblMain.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -205,37 +183,50 @@
             this.tblMain.Margin = new System.Windows.Forms.Padding(0);
             this.tblMain.Name = "tblMain";
             this.tblMain.RowCount = 2;
-            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
             this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tblMain.Size = new System.Drawing.Size(800, 659);
+            this.tblMain.Size = new System.Drawing.Size(711, 527);
             this.tblMain.TabIndex = 2;
             // 
             // titleBar
             // 
             this.titleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(54)))));
+            this.titleBar.CanClose = true;
+            this.titleBar.ControlBox = true;
             this.titleBar.Dock = System.Windows.Forms.DockStyle.Fill;
             this.titleBar.ForeColor = System.Drawing.Color.White;
             this.titleBar.Location = new System.Drawing.Point(0, 0);
             this.titleBar.Margin = new System.Windows.Forms.Padding(0);
             this.titleBar.Name = "titleBar";
-            this.titleBar.Size = new System.Drawing.Size(800, 40);
+            this.titleBar.RememberNormalSize = true;
+            this.titleBar.Size = new System.Drawing.Size(711, 32);
             this.titleBar.TabIndex = 2;
             this.titleBar.Title = "Edit";
             // 
+            // chkMap
+            // 
+            this.chkMap.AutoSize = true;
+            this.chkMap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chkMap.Location = new System.Drawing.Point(103, 51);
+            this.chkMap.Margin = new System.Windows.Forms.Padding(3, 12, 3, 3);
+            this.chkMap.Name = "chkMap";
+            this.chkMap.Size = new System.Drawing.Size(26, 24);
+            this.chkMap.TabIndex = 8;
+            this.chkMap.CheckedChanged += new System.EventHandler(this.chkMap_CheckedChanged);
+            // 
             // ScreenshotEditForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 659);
+            this.ClientSize = new System.Drawing.Size(711, 527);
             this.Controls.Add(this.tblMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ScreenshotEditForm";
             this.Text = "Edit";
             this.tblData.ResumeLayout(false);
             this.tblData.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
-            this.flpMap.ResumeLayout(false);
-            this.flpMap.PerformLayout();
             this.tblMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -253,9 +244,8 @@
         private System.Windows.Forms.TextBox txtTitle;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox cmbMap;
-        private System.Windows.Forms.FlowLayoutPanel flpMap;
-        private System.Windows.Forms.CheckBox chkMap;
         private System.Windows.Forms.TableLayoutPanel tblMain;
         private Controls.TitleBarControl titleBar;
+        private CCheckBox chkMap;
     }
 }
