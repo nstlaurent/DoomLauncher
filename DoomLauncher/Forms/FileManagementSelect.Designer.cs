b896adfd95501a84d7c27cc2d92e2d364b7c47a8
Nick <stlaurent.nicholas@gmail.com>
2023-12-30 05:43:23 -0500

    fix spacing

--- DoomLauncher/Forms/FileManagementSelect.Designer.cs
+++ DoomLauncher/Forms/FileManagementSelect.Designer.cs
@@ -43,18 +43,18 @@
             // 
             this.cmbFileManagement.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbFileManagement.FormattingEnabled = true;
-            this.cmbFileManagement.Location = new System.Drawing.Point(22, 35);
-            this.cmbFileManagement.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbFileManagement.Location = new System.Drawing.Point(10, 28);
+            this.cmbFileManagement.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbFileManagement.Name = "cmbFileManagement";
-            this.cmbFileManagement.Size = new System.Drawing.Size(324, 28);
+            this.cmbFileManagement.Size = new System.Drawing.Size(330, 24);
             this.cmbFileManagement.TabIndex = 0;
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(235, 99);
-            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOK.Location = new System.Drawing.Point(240, 75);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(112, 35);
+            this.btnOK.Size = new System.Drawing.Size(100, 28);
             this.btnOK.TabIndex = 1;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -63,10 +63,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 11);
+            this.label1.Location = new System.Drawing.Point(7, 8);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(333, 20);
+            this.label1.Size = new System.Drawing.Size(273, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Select the management method for the file(s).";
             // 
@@ -81,10 +81,10 @@
             this.tblMain.Margin = new System.Windows.Forms.Padding(0);
             this.tblMain.Name = "tblMain";
             this.tblMain.RowCount = 2;
-            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
             this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tblMain.Size = new System.Drawing.Size(400, 188);
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.tblMain.Size = new System.Drawing.Size(356, 150);
             this.tblMain.TabIndex = 3;
             // 
             // panel1
@@ -93,32 +93,36 @@
             this.panel1.Controls.Add(this.cmbFileManagement);
             this.panel1.Controls.Add(this.btnOK);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 43);
+            this.panel1.Location = new System.Drawing.Point(3, 34);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(394, 142);
+            this.panel1.Size = new System.Drawing.Size(350, 114);
             this.panel1.TabIndex = 0;
             // 
             // titleBar
             // 
             this.titleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(54)))));
+            this.titleBar.CanClose = true;
+            this.titleBar.ControlBox = true;
             this.titleBar.Dock = System.Windows.Forms.DockStyle.Fill;
             this.titleBar.ForeColor = System.Drawing.Color.White;
             this.titleBar.Location = new System.Drawing.Point(0, 0);
             this.titleBar.Margin = new System.Windows.Forms.Padding(0);
             this.titleBar.Name = "titleBar";
-            this.titleBar.Size = new System.Drawing.Size(400, 40);
+            this.titleBar.RememberNormalSize = true;
+            this.titleBar.Size = new System.Drawing.Size(356, 32);
             this.titleBar.TabIndex = 1;
             this.titleBar.Title = "File Management";
             // 
             // FileManagementSelect
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 188);
+            this.ClientSize = new System.Drawing.Size(356, 150);
             this.Controls.Add(this.tblMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FileManagementSelect";
             this.Text = "File Management";
             this.tblMain.ResumeLayout(false);
