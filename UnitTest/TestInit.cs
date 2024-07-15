13daeb0e3e58410b07297908627fc10ef30a6a6d
Ken Scambler <ken.scambler@gmail.com>
2024-07-15 23:17:47 +1000

    Simplified TestInit

--- UnitTest/TestInit.cs
+++ UnitTest/TestInit.cs
@@ -11,13 +11,11 @@ namespace UnitTest
         [AssemblyInitialize()]
         public static void TestInitialize(TestContext testContext)
         {
-            string dataSource = Path.Combine(Directory.GetCurrentDirectory(), "DoomLauncher.sqlite");
             DbDataSourceAdapter adapter = (DbDataSourceAdapter)TestUtil.CreateAdapter();
-            DataAccess access = new DataAccess(new SqliteDatabaseAdapter(), DbDataSourceAdapter.CreateConnectionString(dataSource));
 
             DataCache.Instance.Init(adapter);
 
-            VersionHandler versionHandler = new VersionHandler(access, adapter, new AppConfiguration(adapter));
+            VersionHandler versionHandler = new VersionHandler(adapter.DataAccess, adapter, new AppConfiguration(adapter));
             versionHandler.HandleVersionUpdate();
         }
     }
