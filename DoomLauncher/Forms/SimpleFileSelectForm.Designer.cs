037c5508326ba0e2e7563947a3990914a0b1e426
Nick <stlaurent.nicholas@gmail.com>
2024-10-29 09:19:02 -0400

    use table layout to allow resizing, fixes background not using theme colors

--- DoomLauncher/Forms/SimpleFileSelectForm.Designer.cs
+++ DoomLauncher/Forms/SimpleFileSelectForm.Designer.cs
@@ -32,44 +32,62 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleFileSelectForm));
             this.btnOK = new System.Windows.Forms.Button();
             this.lstFiles = new System.Windows.Forms.ListBox();
+            this.tblMain = new DoomLauncher.TableLayoutPanelDB();
+            this.tblMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnOK
             // 
+            this.btnOK.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(296, 332);
-            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOK.Location = new System.Drawing.Point(270, 311);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(112, 35);
+            this.btnOK.Size = new System.Drawing.Size(100, 28);
             this.btnOK.TabIndex = 1;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
             // 
             // lstFiles
             // 
+            this.lstFiles.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstFiles.FormattingEnabled = true;
-            this.lstFiles.ItemHeight = 25;
-            this.lstFiles.Location = new System.Drawing.Point(18, 18);
-            this.lstFiles.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstFiles.ItemHeight = 20;
+            this.lstFiles.Location = new System.Drawing.Point(4, 4);
+            this.lstFiles.Margin = new System.Windows.Forms.Padding(4);
             this.lstFiles.Name = "lstFiles";
-            this.lstFiles.Size = new System.Drawing.Size(388, 304);
+            this.lstFiles.Size = new System.Drawing.Size(366, 297);
             this.lstFiles.TabIndex = 2;
+            // 
+            // tblMain
+            // 
+            this.tblMain.ColumnCount = 1;
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblMain.Controls.Add(this.btnOK, 0, 1);
+            this.tblMain.Controls.Add(this.lstFiles, 0, 0);
+            this.tblMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblMain.Location = new System.Drawing.Point(0, 0);
+            this.tblMain.Name = "tblMain";
+            this.tblMain.RowCount = 2;
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tblMain.Size = new System.Drawing.Size(374, 345);
+            this.tblMain.TabIndex = 3;
             // 
             // SimpleFileSelectForm
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 375);
-            this.Controls.Add(this.lstFiles);
-            this.Controls.Add(this.btnOK);
+            this.ClientSize = new System.Drawing.Size(374, 345);
+            this.Controls.Add(this.tblMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MaximumSize = new System.Drawing.Size(439, 431);
-            this.MinimumSize = new System.Drawing.Size(439, 431);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SimpleFileSelectForm";
             this.Text = "Select File";
+            this.tblMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -77,5 +95,6 @@
         #endregion
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.ListBox lstFiles;
+        private TableLayoutPanelDB tblMain;
     }
 }
