350a19b11c70f80ac318cba6452289974283114e
Ken Scambler <ken.scambler@gmail.com>
2025-08-05 02:51:44 +1000

    Some cleanup

--- DoomLauncher/Adapters/DataAccess.cs
+++ DoomLauncher/Adapters/DataAccess.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Security.Cryptography;
 
 #pragma warning disable CA2100 //sql injection warning, these all use db parameters anyway
 
--- DoomLauncher/Handlers/ImageExtensions.cs
+++ DoomLauncher/Handlers/ImageExtensions.cs
@@ -3,9 +3,6 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Windows.Media.Effects;
 
 namespace DoomLauncher
 {
@@ -70,8 +67,6 @@ namespace DoomLauncher
             Graphics grPhoto = Graphics.FromImage(bmPhoto);
             grPhoto.Clear(backColor);
             grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
-
-            grPhoto = Graphics.FromImage(bmPhoto);
 
             grPhoto.DrawImage(imgPhoto,
                 new Rectangle(destX, destY, destWidth, destHeight),
@@ -171,63 +166,6 @@ namespace DoomLauncher
                 if (ymin > point.Y) ymin = point.Y;
                 if (ymax < point.Y) ymax = point.Y;
             }
-        }
-
-
-
-        private static Bitmap Blur(Bitmap image, Rectangle rectangle, Int32 blurSize)
-        {
-            Bitmap blurred = new Bitmap(image.Width, image.Height);
-
-            // make an exact copy of the bitmap provided
-            using (Graphics graphics = Graphics.FromImage(blurred))
-                graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
-                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
-
-            // look at every pixel in the blur rectangle
-            for (int xx = rectangle.X; xx < rectangle.X + rectangle.Width; xx++)
-            {
-                for (int yy = rectangle.Y; yy < rectangle.Y + rectangle.Height; yy++)
-                {
-                    int avgR = 0, avgG = 0, avgB = 0;
-                    int blurPixelCount = 0;
-
-                    // average the color of the red, green and blue for each pixel in the
-                    // blur size while making sure you don't go outside the image bounds
-                    for (int x = xx; (x < xx + blurSize && x < image.Width); x++)
-                    {
-                        for (int y = yy; (y < yy + blurSize && y < image.Height); y++)
-                        {
-                            Color pixel = blurred.GetPixel(x, y);
-
-                            avgR += pixel.R;
-                            avgG += pixel.G;
-                            avgB += pixel.B;
-
-                            blurPixelCount++;
-                        }
-                    }
-
-                    avgR = avgR / blurPixelCount;
-                    avgG = avgG / blurPixelCount;
-                    avgB = avgB / blurPixelCount;
-
-                    // now that we know the average for the blur size, set each pixel to that color
-                    for (int x = xx; x < xx + blurSize && x < image.Width && x < rectangle.Width; x++)
-                    {
-                        for (int y = yy; y < yy + blurSize && y < image.Height && y < rectangle.Height; y++)
-                        {
-                            double avg = (avgR + avgG + avgB) / 3.0;
-                            double avgR2 = (avgR * 3.0 + avg) / 4.0;
-                            double avgG2 = (avgG * 3.0 + avg) / 4.0;
-                            double avgB2 = (avgB * 3.0 + avg) / 4.0;
-                            blurred.SetPixel(x, y, Color.FromArgb((int)avgR2, (int)avgG2, (int)avgB2));
-                        }
-                    }
-                }
-            }
-
-            return blurred;
         }
     }
 }
