637f01dab366f9bc75849a364f0e17248c5aa1f8
Ken Scambler <ken.scambler@gmail.com>
2026-05-23 19:24:06 +1000

    Basic fix to upgrade SharpCompress with bad code and not enough tests

--- DoomLauncher/Archive/Rar/RarArchiveReader.cs
+++ DoomLauncher/Archive/Rar/RarArchiveReader.cs
@@ -6,13 +6,20 @@ namespace DoomLauncher.Archive.Rar
 {
     public class RarArchiveReader : IArchiveReader
     {
-        private readonly RarArchive m_archive;
+        private readonly IRarArchive m_archive;
         private readonly RarArchiveEntry[] m_entries;
 
         public RarArchiveReader(string file)
         {
-            m_archive = RarArchive.Open(file);
-            m_entries = m_archive.Entries.Where(x => RarArchiveEntry.IsValid(x)).Select(x => new RarArchiveEntry(x)).ToArray();
+            m_archive = RarArchive.OpenArchive(file);
+            m_entries = m_archive.Entries.Where(x =>
+            {
+                if (x is SharpCompress.Archives.Rar.RarArchiveEntry)
+                {
+                    return RarArchiveEntry.IsValid(x as SharpCompress.Archives.Rar.RarArchiveEntry);
+                }
+                return false;
+            }).Select(x => new RarArchiveEntry(x as SharpCompress.Archives.Rar.RarArchiveEntry)).ToArray();
         }
 
         public IEnumerable<IArchiveEntry> Entries => m_entries;
