72e84f5c12fed8170d2616707dcec426b642412a
HazNut <65128084+HazNut@users.noreply.github.com>
2026-05-26 18:06:50 +0100

    Add and update unit tests for Steam loading functionality

--- UnitTest/Tests/TestSteamFileUtils.cs
+++ UnitTest/Tests/TestSteamFileUtils.cs
@@ -8,7 +8,7 @@ namespace UnitTest.Tests
     public class TestSteamFileUtils
     {
         [TestMethod]
-        public void TryGetInstallDir()
+        public void TryGetInstallDir_ReturnsTrueAndInstallDirWhenAcfIsValid()
         {
             var path = Path.GetFullPath(@"Resources\TestSteamLibrary1\steamapps\appmanifest_2280.acf");
             var success = SteamFileUtils.TryGetInstallDir(path, out var installDir);
@@ -18,7 +18,47 @@ namespace UnitTest.Tests
         }
 
         [TestMethod]
-        public void TryGetLibraryPaths()
+        public void TryGetInstallDir_ReturnsFalseAndNullWhenAcfDoesNotExist()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamLibrary1\steamapps\appmanifest_2360.acf");
+            var success = SteamFileUtils.TryGetInstallDir(path, out var installDir);
+
+            Assert.IsFalse(success);
+            Assert.IsNull(installDir);
+        }
+
+        [TestMethod]
+        public void TryGetInstallDir_ReturnsFalseAndNullWhenAcfIsInvalid()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamLibrary3\steamapps\appmanifest_2280.acf");
+            var success = SteamFileUtils.TryGetInstallDir(path, out var installDir);
+
+            Assert.IsFalse(success);
+            Assert.IsNull(installDir);
+        }
+
+        [TestMethod]
+        public void TryGetInstallDir_ReturnsFalseAndNullWhenInstallDirIsNotPresent()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamLibrary3\steamapps\appmanifest_2360.acf");
+            var success = SteamFileUtils.TryGetInstallDir(path, out var installDir);
+
+            Assert.IsFalse(success);
+            Assert.IsNull(installDir);
+        }
+
+        [TestMethod]
+        public void TryGetInstallDir_ReturnsFalseAndNullWhenInstallDirIsInvalid()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamLibrary3\steamapps\appmanifest_2390.acf");
+            var success = SteamFileUtils.TryGetInstallDir(path, out var installDir);
+
+            Assert.IsFalse(success);
+            Assert.IsNull(installDir);
+        }
+
+        [TestMethod]
+        public void TryGetLibraryPaths_ReturnsTrueAndLibraryPathsWhenVdfIsValid()
         {
             var path = Path.GetFullPath(@"Resources\TestSteamInstall\config\libraryfolders.vdf");
             var success = SteamFileUtils.TryGetLibraryPaths(path, out var libraryPaths);
@@ -27,6 +67,46 @@ namespace UnitTest.Tests
             Assert.AreEqual(2, libraryPaths.Count);
             Assert.AreEqual(@"Resources\TestSteamLibrary1", libraryPaths[0]);
             Assert.AreEqual(@"Resources\TestSteamLibrary2", libraryPaths[1]);
+        }
+
+        [TestMethod]
+        public void TryGetLibraryPaths_ReturnsFalseAndNullWhenVdfDoesNotExist()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamInstall2\config\libraryfolders.vdf");
+            var success = SteamFileUtils.TryGetLibraryPaths(path, out var libraryPaths);
+
+            Assert.IsFalse(success);
+            Assert.IsNull(libraryPaths);
+        }
+
+        [TestMethod]
+        public void TryGetLibraryPaths_ReturnsFalseAndNullWhenVdfIsInvalid()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamInstall3\config\libraryfolders.vdf");
+            var success = SteamFileUtils.TryGetLibraryPaths(path, out var libraryPaths);
+
+            Assert.IsFalse(success);
+            Assert.IsNull(libraryPaths);
+        }
+
+        [TestMethod]
+        public void TryGetLibraryPaths_ReturnsFalseAndNullWhenPathIsNotPresent()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamInstall4\config\libraryfolders.vdf");
+            var success = SteamFileUtils.TryGetLibraryPaths(path, out var libraryPaths);
+
+            Assert.IsFalse(success);
+            Assert.IsNull(libraryPaths);
+        }
+
+        [TestMethod]
+        public void TryGetLibraryPaths_ReturnsFalseAndNullWhenPathIsInvalid()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamInstall5\config\libraryfolders.vdf");
+            var success = SteamFileUtils.TryGetLibraryPaths(path, out var libraryPaths);
+
+            Assert.IsFalse(success);
+            Assert.IsNull(libraryPaths);
         }
     }
 }
--- UnitTest/Tests/TestSteamLoader.cs
+++ UnitTest/Tests/TestSteamLoader.cs
@@ -9,12 +9,39 @@ namespace UnitTest.Tests
     public class TestSteamLoader
     {
         [TestMethod]
-        public void GetGameFolder()
+        public void GetGameFolder_ReturnsGameFolderIfGameIsInstalled()
         {
             var path = Path.GetFullPath(@"Resources\TestSteamInstall");
             var gameFolder = SteamLoader.GetGameFolder(path, StoreGame.ULTIMATE_DOOM);
 
             Assert.AreEqual(@"Resources\TestSteamLibrary1\steamapps\common\TestDoom", gameFolder);
+        }
+
+        [TestMethod]
+        public void GetGameFolder_ReturnsNullIfInstallFolderDoesNotExist()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamInstall0");
+            var gameFolder = SteamLoader.GetGameFolder(path, StoreGame.ULTIMATE_DOOM);
+
+            Assert.IsNull(gameFolder);
+        }
+
+        [TestMethod]
+        public void GetGameFolder_ReturnsNullIfNoLibraryPathsFound()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamInstall2");
+            var gameFolder = SteamLoader.GetGameFolder(path, StoreGame.ULTIMATE_DOOM);
+
+            Assert.IsNull(gameFolder);
+        }
+
+        [TestMethod]
+        public void GetGameFolder_ReturnsNullIfGameDirectoryNotFound()
+        {
+            var path = Path.GetFullPath(@"Resources\TestSteamInstall");
+            var gameFolder = SteamLoader.GetGameFolder(path, StoreGame.DOOM64);
+
+            Assert.IsNull(gameFolder);
         }
     }
 }
