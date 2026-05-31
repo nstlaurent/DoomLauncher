6df3a112a548ec6e4b75bd9b12bab0e1e4103951
Ken Scambler <ken.scambler@gmail.com>
2026-05-31 23:29:56 +1000

    Try upgrading System.Memory to 4.0.1.2

---
+++ UnitTest/Tests/TestLegacyTitlePicMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Tests
+{
+    [TestClass]
+    public class TestLegacyTitlePicMigration
+    {
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteScreenshotThatIsReallyATitlePic_DeletesTitlePicScreenshot()
+        {
+
+        }
+
+    }
+}
